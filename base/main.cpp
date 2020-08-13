3a16e5238533ce6fcf3bf5149f29ffe5d4b47f25
Zhangzhenyu1996 <13690122241@163.com>
2020-08-13 21:38:41 +0800

    修改Cmake文件及添加build.sh脚本

--- base/main.cpp
+++ base/main.cpp
@@ -4,8 +4,8 @@
 #include <bits/sigaction.h>
 #include "util.h"
 #include "dbg.h"
-
-#define CONF "init.conf"
+#include "EventLoop.h"
+#define CONF "../../init.conf"
 
 char* confFile = (char*)CONF;
 Zconf conf;
@@ -17,7 +17,9 @@ int main()
     assert(ReadConf(confFile, &conf) != -1);
     printf("root:%s, port:%d, threadNum:%d\n", conf.root, conf.port, conf.threadNum);
     
-    //；处理SIGPIPE信号
+    //处理SIGPIPE信号,如果客户端关闭套接字close,
+    //而服务器调用了一次write，服务器会结构到一个RST segment，
+    //如果服务器再次调用了write，这个时候会产生SIGPIPE信号，终止程序
     struct sigaction sa;
     memset(&sa, '\0', sizeof(sa));
     sa.sa_handler = SIG_IGN;
@@ -36,7 +38,7 @@ int main()
     check(rc == 0, "set socket non blocking");
 
     //创建epoll和注册监听fd
-    
+    // EventLoop mainLoop;
 
     return 0;
 }
